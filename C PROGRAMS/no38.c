2a9f8be6d218e797bcd4d1af8d7bc8af8f1cd0f4
DEBNANDAN BERA <116198506+Debnandan@users.noreply.github.com>
2023-05-17 19:19:46 +0530

    Add files via upload
    adding answers

---
+++ C PROGRAMS/no38.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+   int num[]={9,6,1,3,7}, i, j ,temp;
+   for (i = 0; i < 5; i++)
+   {
+      for (j = i + 1; j < 5; j++)
+      {
+         if (num[i] > num[j])
+         {
+            temp = num[i];
+            num[i] = num[j];
+            num[j] = temp;
+         }
+      }
+   }
+   printf("max = %d",num[5]);
+   return 0;
+}
